f51c868725f05a5bdb7105f420c67ca82deeb259
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-25 20:01:32 +0200

    Important things rendered on top

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : Interactable
+{
+    [SerializeField] Animator anim;
+
+    bool isOpen = false;
+
+    void Start()
+    {
+        function = MoveDrawer;
+    }
+
+    void MoveDrawer()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen)
+            anim.Play("DrawerOpen");
+        else
+            anim.Play("DrawerClose");
+
+    }
+}
