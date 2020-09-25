beb7e404b54b65080a7b138c15f4bbdac615814a
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-25 20:33:14 +0200

    Added Rubber band shooter

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab = null;
+
+    [SerializeField] float timeBetweenShots = 0.2f;
+    float timer = 0;
+
+    [SerializeField] float bulletSpeed = 5f;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenShots && Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
+
+            bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+
+            Destroy(bullet, 2f);
+            timer = 0;
+        }
+    }
+}
