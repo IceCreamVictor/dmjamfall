06409f340976c190b920dc94cf2f0768aab2ef32
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-25 17:33:03 +0200

    Smasher added

--- Assets/Scripts/PlayerSmash.cs
+++ Assets/Scripts/PlayerSmash.cs
@@ -7,31 +7,36 @@ public class PlayerSmash : MonoBehaviour
     [SerializeField] Animator anim = null;
     [SerializeField] Transform smasher = null;
     Vector3 newPos = Vector3.zero;
+    Vector3 defaultPos = Vector3.zero;
 
-
-    void Start()
+    private void Start()
     {
-        
+        defaultPos = transform.localPosition;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         if(newPos != Vector3.zero)
         {
-
+            smasher.position = Vector3.Lerp(smasher.position, newPos, 0.2f);
+        }
+        else
+        {
+            smasher.localPosition = defaultPos;
         }
 
-
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(Input.GetMouseButtonDown(0))
         {
-            Ray ray = GetComponent<Camera>().ScreenPointToRay(new Vector2(0.5f, 0.5f));
+            Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
             RaycastHit hit;
 
             if(Physics.Raycast(ray, out hit))
             {
-                if (hit.distance < 2f)
+                if (hit.distance < 2.5f)
                 {
-
+                    newPos = hit.point;
+                    newPos.y += 0.5f;
                 }
             }
 
@@ -41,6 +46,6 @@ public class PlayerSmash : MonoBehaviour
 
     public void StopMoving()
     {
-        
+        newPos = Vector3.zero;
     }
 }
