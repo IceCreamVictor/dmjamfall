f97a2532ade62b87c324c8adb8aff254ff1f886d
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-25 18:21:41 +0200

    Door Open

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    [SerializeField] Animator anim;
+    bool isOpen = false;
+
+    public bool canOpen = false;
+
+    void Start()
+    {
+        function = OpenDoor;   
+    }
+    void OpenDoor()
+    {
+        if (!isOpen && canOpen)
+        {
+            isOpen = true;
+            anim.Play("DoorOpen");
+        }
+    }
+}
