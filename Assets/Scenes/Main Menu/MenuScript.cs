48b1d137a960081cbc947187afb2cdd835ef7d8c
vic-chan <vic-chan@chan.local>
2020-09-26 23:42:47 +0200

    Main Menu

---
+++ Assets/Scenes/Main Menu/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] int levelToStart;
+    
+
+    public void StartGame(){
+        SceneManager.LoadScene(levelToStart);
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+}
