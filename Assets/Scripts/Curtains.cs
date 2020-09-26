501635de8a1f6ea68792f352532e7af34f7fc6d7
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-26 19:07:51 +0200

    Added bathroom interaction

---
+++ Assets/Scripts/Curtains.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtains : Interactable
+{
+    [SerializeField] Animator anim = null;
+    bool isOpen = false;
+
+    void Start()
+    {
+        function = MoveCurtains;
+    }
+
+    void MoveCurtains()
+    {
+        if (isOpen)
+            return;
+
+        Collider[] cols = GetComponents<Collider>();
+
+        for (int i = 0; i < cols.Length; i++)
+        {
+            cols[i].enabled = false;
+        }
+
+        isOpen = true;
+        anim.Play("CurtainsMove");
+    }
+}
---
+++ Assets/Scripts/ToiletLid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletLid : Interactable
+{
+    [SerializeField] Animator anim = null;
+    bool isOpen = false;
+    void Start()
+    {
+        function = MoveLid;
+    }
+
+    void MoveLid()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen)
+            anim.Play("LidOpen");
+        else
+            anim.Play("LidClose");
+
+    }
+}
