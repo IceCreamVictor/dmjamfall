e29c20cc375998418d8cdc70c0c7d65fd6c22f08
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-26 15:04:33 +0200

    TextMeshPro

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public static float time = 0;
+    public static float highScore = 0;
+
+    public static bool isStarted = false;
+
+    void Start()
+    {
+        time = 0;
+        highScore = PlayerPrefs.GetFloat("HighScore");
+    }
+
+    void Update()
+    {        
+        if(isStarted)
+            time += Time.deltaTime; 
+    }
+}
