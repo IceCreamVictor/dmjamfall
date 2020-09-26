ec933da53acb61cf7908ca111734c3e54513e23c
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-26 13:32:18 +0200

    Added Radio Control & AudioManager

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Radio : Interactable
 {
-    [SerializeField] AudioClip[] songs = null;
+    int curSong = 0;
     void Start()
     {
+        AudioManager.instance.Play("Static");
         function = PlayRadio;
     }
 
     void PlayRadio()
     {
+        curSong++;
+
+        switch(curSong)
+        {
+            case 1:
+                AudioManager.instance.Stop("Static");
+                break;
+
+            default:
+                AudioManager.instance.Play("Static");
+                curSong = 0;
+                break;
+        }
+
         
     }
 }
