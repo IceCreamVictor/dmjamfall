f6fcc5ac24314bba9eb619c59400052c0ae435e0
vic-chan <vic-chan@chan.local>
2020-09-26 02:35:30 +0200

    backup

--- Assets/Scripts/Svensker/SvenskerMovement.cs
+++ Assets/Scripts/Svensker/SvenskerMovement.cs
@@ -40,31 +40,22 @@ public class SvenskerMovement : MonoBehaviour
     }
    public IEnumerator WalkInCircles(Transform pos, float radius)
    {    
+       Debug.Log("He");
         int thetaMax = 8;
         for(int theta = 0; theta <= thetaMax; theta++)
         {
+            yield return  new WaitForSeconds(0.2f);
             Vector3 newPosition = new Vector3(pos.position.x + radius * Mathf.Cos(theta), pos.position.y, pos.position.z + radius * Mathf.Sin(theta));
+            Debug.Log(newPosition);
             agent.destination = newPosition; 
         }
-
-            yield return  new WaitForSeconds(0.2f);
-
-
-
    }
-
-
     IEnumerator StartRun(){
-        
-
         yield return new WaitForSeconds(timeBeforeRun);
         agent.destination = goal.position;
-
-
-    }
+        }
 
     bool CheckDestinationReached() {
-
         float distanceToTarget = Vector3.Distance(transform.position, goal.position);
         
         if(distanceToTarget < destinationReachedTreshold)
