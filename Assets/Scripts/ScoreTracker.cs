e01db7a29c1523b6834aadea73f2890daa32f9dc
Cyandog <46567276+Baltzer77@users.noreply.github.com>
2020-09-27 15:38:15 +0200

    Idk

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreTracker : MonoBehaviour
 {
@@ -8,16 +9,27 @@ public class ScoreTracker : MonoBehaviour
     public static float highScore = 0;
 
     public static bool isStarted = false;
+    public static bool hasCompleted = false;
+
+    [SerializeField] TextMeshProUGUI timeText = null;
 
     void Start()
     {
+        hasCompleted = PlayerPrefs.HasKey("Completed");
+
         time = 0;
         highScore = PlayerPrefs.GetFloat("HighScore");
     }
 
     void Update()
-    {        
-        if(isStarted)
-            time += Time.deltaTime; 
+    {
+        if (isStarted)
+        {
+            time += Time.deltaTime;
+            if(hasCompleted)
+            {
+                timeText.text = time.ToString();
+            }
+        }       
     }
 }
